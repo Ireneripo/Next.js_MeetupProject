596788d0499f0c0a6d23b78dd9f76f4bfbc3ad5e
Irene Riportella <riportellairene@gmail.com>
2022-11-04 17:44:17 +0100

    added metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,4 +1,6 @@
 // our-domain.com/new-meetup
+import { Fragment } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
@@ -19,7 +21,18 @@ function NewMeetupPage() {
     router.push('/');
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups, create amazing networking opportunities!"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
